4cb1e60b554ca93adbefdf2953b9908796bbc2e1
b115030 <iitiangokul@gmail.com>
2020-10-19 08:40:27 +0530

    Added first name check regex

---
+++ UserRegistration.java
@@ -0,0 +1,9 @@
+
+public class UserRegistration {
+
+	public static void main(String[] args) {
+		UserRegistrationImpl userRegister = new UserRegistrationImpl();
+		userRegister.takeUserInput();
+	}
+
+}
---
+++ UserRegistrationImpl.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegistrationImpl {
+	public Pattern firstNamePattern = Pattern.compile("[A-Z][a-z]{2,}");
+	public boolean firstNameCheck( String firstName) {
+		Matcher match = firstNamePattern.matcher(firstName);
+        if(match.matches()){
+            return true;
+        }
+        return false;
+	}
+	public void takeUserInput() {
+		boolean flag;
+		Scanner readIn = new Scanner(System.in);
+		System.out.println("Enter a Valid first name(Starts with a Cap and has min 3 letters)");
+		String firstName = readIn.next();
+		flag = firstNameCheck(firstName);
+		checkValidity(flag);
+	}
+	public void checkValidity(boolean flag) {
+		if (flag == true) {
+			System.out.println("Valid");
+			return;
+		}
+		System.out.println("Invalid");
+		return;
+	}
+}
